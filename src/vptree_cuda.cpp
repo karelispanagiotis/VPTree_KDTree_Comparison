c33babeaed8fa67ad936eca25ef09577fb1c9fd4
Karelis Panagiotis <karelispanagiotis@gmail.com>
2020-02-03 15:26:46 +0200

    First Implementation of CUDA, passes tester with some floating point error

--- src/vptree_cuda.cpp
+++ src/vptree_cuda.cpp
@@ -1,20 +1,33 @@
 #include "vptree.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include <cuda.h>
 #include <cuda_runtime_api.h>
 #include <math.h>
 
 #define BLK_SZ 512
 
+#define cudaErrChk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+
 //Globally defined variables for easy data access by functions
-__managed__ int *dev_idArr;
-__managed__ float *dev_distArr;
-__managed__ float *dev_X;
-__managed__ vptree *dev_treeArr;
-__managed__ int dev_n, dev_d;
+__device__ __managed__ int *dev_idArr;
+__device__ __managed__ float *dev_distArr;
+__device__ __managed__ float *dev_X;
+__device__ __managed__ vptree *dev_treeArr;
+__device__ __managed__ int dev_n, dev_d;
+
 
 __device__ __forceinline__
 float sqr(float x) {return x*x;}
+
 
 __global__ 
 void distCalc(float *vp, int start, int end)
@@ -65,11 +78,12 @@ void quickSelect(int kpos, int start, int end)
 }
 
 __global__
-void recursiveBuildTree(int start, int end, int nodeNumber)
+void recursiveBuildTree(int start, int end, int nodeNumber, float *hostPtr_d, vptree *hostPtr_t )
 {
     //consider X[ idArr[end] ] as vantage point
+    float *vp = &dev_X[ dev_idArr[end]*dev_d ]; 
     dev_treeArr[nodeNumber].idx = dev_idArr[end];
-    dev_treeArr[nodeNumber].vp = &dev_X[ dev_idArr[end]*dev_d ];
+    dev_treeArr[nodeNumber].vp = &hostPtr_d[ dev_idArr[end]*dev_d ];
 
     if(start == end)
     {
@@ -78,61 +92,60 @@ void recursiveBuildTree(int start, int end, int nodeNumber)
         return;
     }
     end--; //end is the vantage point, we're not dealing with it again
-
-    distCalc<<<(end-start+BLK_SZ)/BLK_SZ, BLK_SZ>>>(dev_treeArr[nodeNumber].vp, start, end);
+    
+    distCalc<<<(end-start+BLK_SZ)/BLK_SZ, BLK_SZ>>>(vp, start, end);
     cudaDeviceSynchronize();
 
     quickSelect((start+end)/2, start, end);
-
+    
     dev_treeArr[nodeNumber].md = sqrtf(dev_distArr[ (start+end)/2 ]);
-    dev_treeArr[nodeNumber].inner = &dev_treeArr[2*nodeNumber + 1];
-    dev_treeArr[nodeNumber].outer = &dev_treeArr[2*nodeNumber + 2];
+    dev_treeArr[nodeNumber].inner = &hostPtr_t[2*nodeNumber + 1];
+    dev_treeArr[nodeNumber].outer = &hostPtr_t[2*nodeNumber + 2];
 
-    recursiveBuildTree<<<1,1>>>(start, (start+end)/2, 2*nodeNumber + 1);
+    recursiveBuildTree<<<1,1>>>(start, (start+end)/2, 2*nodeNumber + 1, hostPtr_d, hostPtr_t);
     if(end>start)
-        recursiveBuildTree<<<1,1>>>((start+end)/2 + 1, end, 2*nodeNumber + 2);
+        recursiveBuildTree<<<1,1>>>((start+end)/2 + 1, end, 2*nodeNumber + 2, hostPtr_d, hostPtr_t);
     else dev_treeArr[nodeNumber].outer = NULL;
-    cudaDeviceSynchronize();
-
 }
 
 __global__ void idx_init()
 {
     int idx = blockIdx.x*BLK_SZ + threadIdx.x;
-    if(idx<dev_n);
-        dev_idArr[idx] = idx; 
+    if(idx<dev_n)
+        dev_idArr[idx] = idx;
 }
 
 vptree *buildvp(float *X, int n, int d)
 {
     size_t treeSize = 1<<(32 - __builtin_clz(n-1)); // next greater power of 2 than n
+    vptree *treeArr = (vptree *)malloc(treeSize*sizeof(vptree));
 
     // Allocate Memory on Device
-    cudaMalloc(&dev_X, n*d*sizeof(float));
-    cudaMalloc(&dev_idArr, n*sizeof(int));
-    cudaMalloc(&dev_distArr, n*sizeof(float));
-    cudaMalloc(&dev_treeArr, treeSize*sizeof(vptree));
+    cudaErrChk( cudaMalloc(&dev_X, n*d*sizeof(float)) );
+    cudaErrChk( cudaMalloc(&dev_idArr, n*sizeof(int)) );
+    cudaErrChk( cudaMalloc(&dev_distArr, n*sizeof(float)) );
+    cudaErrChk( cudaMalloc(&dev_treeArr, treeSize*sizeof(vptree) ));
 
     // Initialise Device Variables
-    cudaMemcpy(dev_X, X, n*d*sizeof(int), cudaMemcpyHostToDevice); //copy Data
-    cudaMemset(&dev_n, n, sizeof(int)); //Set n
-    cudaMemset(&dev_d, d, sizeof(int)); //Set d
+    cudaErrChk(cudaMemcpy(dev_X, X, n*d*sizeof(int), cudaMemcpyHostToDevice)); //copy Data
+    dev_n = n; //Set n
+    dev_d = d; //Set d
     idx_init<<<(n+BLK_SZ-1)/BLK_SZ, BLK_SZ>>>(); //set idx [0...n-1]
     cudaDeviceSynchronize();
     
     // Recursively build tree in GPU
-    recursiveBuildTree<<<1,1>>>(0, n-1, 0); //This kernel only needs one thread
+    recursiveBuildTree<<<1,1>>>(0, n-1, 0, X, treeArr); //This kernel only needs one thread
+    cudaDeviceSynchronize();
 
     // Copy the result back to host
-    vptree *treeArr = (vptree *)malloc(treeSize*sizeof(vptree));
-    cudaMemcpy(treeArr, dev_treeArr, treeSize*sizeof(vptree), cudaMemcpyDeviceToHost);
+    cudaErrChk(cudaMemcpy(treeArr, dev_treeArr, treeSize*sizeof(vptree), cudaMemcpyDeviceToHost));
 
     // Clean-up
-    cudaFree(dev_X);
-    cudaFree(dev_idArr);
-    cudaFree(dev_distArr);
-    cudaFree(dev_treeArr);
-    
+    cudaErrChk(cudaFree(dev_X));
+    cudaErrChk(cudaFree(dev_idArr));
+    cudaErrChk(cudaFree(dev_distArr));
+    cudaErrChk(cudaFree(dev_treeArr));
+
     // Return
     return &treeArr[0];
 }
@@ -140,6 +153,6 @@ vptree *buildvp(float *X, int n, int d)
 /////////////////////////////////////////////////////////////////////////////
 vptree* getInner(vptree* T){    return T->inner;}
 vptree* getOuter(vptree* T){    return T->outer;}
-float getMD(vptree* T)    {    return T->md;}  
-float* getVP(vptree* T)   {    return T->vp;}
-int getIDX(vptree* T)      {    return T->idx;}
+float getMD(vptree* T)     {    return T->md;   }  
+float* getVP(vptree* T)    {    return T->vp;   }
+int getIDX(vptree* T)      {    return T->idx;  }
