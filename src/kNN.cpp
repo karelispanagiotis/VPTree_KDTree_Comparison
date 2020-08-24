e17b283958182a1f3469da198fa87a6507b395b4
Panagiotis Karelis <karelispanagiotis@gmail.com>
2020-08-24 13:29:52 +0300

    Removed global vars & code clean-up on kNN.cpp

--- src/kNN.cpp
+++ src/kNN.cpp
@@ -3,8 +3,9 @@
 #include <math.h>
 #include <cstdio>
 
-int nodesVisited;
+using namespace std;
 
+// Helper struct used in kNN search algorithm
 typedef struct neighbour
 {
     float dist; //!< Distance to some query point 
@@ -12,8 +13,6 @@ typedef struct neighbour
 
 }neighbour;
 bool operator<(const neighbour &n1, const neighbour &n2){ return n1.dist < n2.dist; }
-
-std::priority_queue<neighbour> queue;
 
 float dist(float *X, float *Y, int dim)
 {
@@ -24,28 +23,27 @@ float dist(float *X, float *Y, int dim)
 }
 
 // The top of queue always keeps the farthest neighbour
-void vpt_search(vptree *node, float *query, int d, int k)
+void vpt_search(priority_queue<neighbour> &q, vptree *node, float *query, int d, int k, int &nodesVisited)
 {
-    if(node == NULL)
-        return;
-    
+    if(node == NULL) return;
+
+    nodesVisited++;
     neighbour x = {dist(node->vp, query, d), node->idx};
 
-    if(queue.size() < k)
-        queue.push(x);
-    else if(x.dist < queue.top().dist)
+    if(q.size() < k)
+        q.push(x);
+    else if(x.dist < q.top().dist)
     {
-        queue.pop();
-        queue.push(x);
+        q.pop();
+        q.push(x);
     }
 
-    if(x.dist <= queue.top().dist + node->md)
-        vpt_search(node->inner, query, d, k);
+    if(x.dist <= q.top().dist + node->md)
+        vpt_search(q, node->inner, query, d, k, nodesVisited);
     
-    if(x.dist >= node->md - queue.top().dist)
-        vpt_search(node->outer, query, d, k);
+    if(x.dist >= node->md - q.top().dist)
+        vpt_search(q, node->outer, query, d, k, nodesVisited);
 }
-
 
 knnresult vptree_kNN(vptree *root, float *query, int n, int d, int k)
 {
@@ -54,44 +52,45 @@ knnresult vptree_kNN(vptree *root, float *query, int n, int d, int k)
     result.ndist = (float *)malloc(n*k*sizeof(float));
     result.nidx  = (int *)  malloc(n*k*sizeof(int));
 
-    neighbour x;
+    int nodesVisited = 0;    
     for(int i=0; i<n; i++)
     {
-        vpt_search(root, query + i*d, d, k);
+        priority_queue<neighbour> q;
+        vpt_search(q, root, query + i*d, d, k, nodesVisited);
         for(int j=k-1; j>=0; j--)
         {
-            x = queue.top();
+            neighbour x = q.top(); q.pop();
             result.nidx[i*k + j] = x.index;
             result.ndist[i*k + j] = x.dist;
-            queue.pop();
         }
     }
+    printf("Average nodes: %lf\n", (double)nodesVisited/n);
 
     return result;
 }
 
 ////////////////////////////////////////////////////////////////////////////
 
-void kdt_search(kdtree *node, float *query, int d, int k)
+void kdt_search(priority_queue<neighbour> &q, kdtree *node, float *query, int d, int k, int &nodesVisited)
 {
-    if(node==NULL)
-        return;
+    if(node==NULL) return;
+
     nodesVisited++;
     neighbour x = {dist(node->p, query, d), node->idx};
 
-    if(queue.size() < k)
-        queue.push(x);
-    else if(x.dist < queue.top().dist)
+    if(q.size() < k)
+        q.push(x);
+    else if(x.dist < q.top().dist)
     {
-        queue.pop();
-        queue.push(x);
+        q.pop();
+        q.push(x);
     }
 
     int axis = getAxis(node);
-    if(node->mc <= queue.top().dist + query[axis])
-        kdt_search(getRight(node), query, d, k);
-    if(node->mc >= query[axis] - queue.top().dist)
-        kdt_search(getLeft(node), query, d, k);
+    if(node->mc <= q.top().dist + query[axis])
+        kdt_search(q, node->right, query, d, k, nodesVisited);
+    if(node->mc >= query[axis] - q.top().dist)
+        kdt_search(q, node->left, query, d, k, nodesVisited);
 }
 
 knnresult kdtree_kNN(kdtree *root, float *query, int n, int d, int k)
@@ -101,19 +100,19 @@ knnresult kdtree_kNN(kdtree *root, float *query, int n, int d, int k)
     result.ndist = (float *)malloc(n*k*sizeof(float));
     result.nidx  = (int *)  malloc(n*k*sizeof(int));
 
-    neighbour x;
+    int nodesVisited = 0;
     for(int i=0; i<n; i++)
     {
-        kdt_search(root, query + i*d, d, k);
+        priority_queue<neighbour> q;
+        kdt_search(q, root, query + i*d, d, k, nodesVisited);
         for(int j=k-1; j>=0; j--)
         {
-            x = queue.top();
+            neighbour x = q.top(); q.pop();
             result.nidx[i*k + j] = x.index;
             result.ndist[i*k + j] = x.dist;
-            queue.pop();
         }
     }
-    printf("Total %d nodes visited\n", nodesVisited);
+    printf("Average nodes: %lf\n", (double)nodesVisited/n);
 
     return result;
 }
